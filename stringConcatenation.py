7c2707edc47c7cb4563afdd1bbf555f757ebd961
VayanandanaBuddavarapu <vayanandanabud@gmail.com>
2026-01-09 18:23:44 +0530

    Add files via upload

---
+++ stringConcatenation.py
@@ -0,0 +1,7 @@
+# Joining strings together is called string concatenation.
+# Using + operator allows us to concatenation or join strings easily.
+a = "Hello"
+b = "World"
+
+result = a + " " + b
+print(result)
